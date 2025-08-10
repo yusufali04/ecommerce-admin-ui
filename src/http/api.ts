a373d7ec125ad7ac1845c83ad213eb4da0524cd0
Yusuf <yusufali.5094@gmail.com>
2025-08-10 21:11:17 +0530

    add api gateway

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,13 +1,15 @@
 import { Credentials, Tenant, TenantFormValues, UserFormValues } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+// const CATALOG_SERVICE = "/api/catalog";
 // Auth service
-export const login = (credentials: Credentials) => api.post('/auth/login', credentials);
-export const self = () => api.get('/auth/self');
-export const logout = () => api.post('/auth/logout');
-export const getUsers = (queryString: string) => api.get(`/users?${queryString}`);
-export const getTenants = (queryString: string) => api.get(`/tenants?${queryString}`);
-export const updateTenant = (tenantId: string, tenantData: TenantFormValues) => api.patch(`/tenants/${tenantId}`, tenantData);
-export const createTenant = (tenantData: Tenant) => api.post('/tenants', tenantData);
-export const createUser = (userData: UserFormValues) => api.post('/users', userData);
-export const updateUser = (userId: string, userData: UserFormValues) => api.patch(`/users/${userId}`, userData);
+export const login = (credentials: Credentials) => api.post(`${AUTH_SERVICE}/auth/login`, credentials);
+export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
+export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
+export const getUsers = (queryString: string) => api.get(`${AUTH_SERVICE}/users?${queryString}`);
+export const getTenants = (queryString: string) => api.get(`${AUTH_SERVICE}/tenants?${queryString}`);
+export const updateTenant = (tenantId: string, tenantData: TenantFormValues) => api.patch(`${AUTH_SERVICE}/tenants/${tenantId}`, tenantData);
+export const createTenant = (tenantData: Tenant) => api.post(`/tenants`, tenantData);
+export const createUser = (userData: UserFormValues) => api.post(`/users`, userData);
+export const updateUser = (userId: string, userData: UserFormValues) => api.patch(`${AUTH_SERVICE}/users/${userId}`, userData);
